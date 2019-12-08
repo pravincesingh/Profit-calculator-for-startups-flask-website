4fe160541e41bc7a2b2c94831917a6ea2a5bec4e
Pravince Kumar <pravincesingh@gmail.com>
2019-12-08 17:02:10 +0530

    Adding backend file

--- app.py
+++ app.py
@@ -5,35 +5,34 @@ import numpy as np
 
 app =Flask(__name__)
 
-mul_reg =oepn("multiple_linear_regression.pkl","rb")
+mul_reg =open("multiple_linear_model.pkl","rb")
 ml_model = joblib.load(mul_reg)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('first.html')
 
-@app.route("/predict",method =['GET','POST'])
+@app.route("/predict", methods=['GET', 'POST'])
 def predict():
     if request.method =="POST":
         try:
             NewYork = float(request.form['NewYork'])
             California =float(request.form['California'])
             Florida = float(request.form['Florida'])
-            RnDSpend =request.form['RnDSpend']
-            AdminSpend = request.form['AdminSpend']
-            MarketSpend= request.form['MarketSpend']
-            pred_args = [NewYork,California,Florida,RnDSpend,AdminSpend,MarketSpend]
+            RnD_Spend =float(request.form['RnD_Spend'])
+            Admin_Spend = float(request.form['Admin_Spend'])
+            Market_Spend= float(request.form['Market_Spend'])
+            pred_args = [NewYork,California,Florida,RnD_Spend,Admin_Spend,Market_Spend]
             pred_args_arr=np.array(pred_args)
             pred_args_arr=pred_args_arr.reshape(1,-1)
             # mul_reg =oepn("multiple_linear_regression.pkl","rb")
             # ml_model = joblib.load(mul_reg)
             model_prediction = ml_model.predict(pred_args_arr)
-            model_prediction= round(flaot(model_prediction),2)
-
-        exception ValueError:
+            model_prediction= round(float(model_prediction),2)
+        except ValueError:
             return "Please check if the value are entered correctly"
 
-    return rander_template('predict.html',prediction= mdoel_prediction )
+    return render_template("predict.html",prediction= model_prediction )
 
 if __name__ == "__main__":
     app.run()
